3c9690b4424c434c96084ee0eb4de53efa6045b9
k.kyzylool <k.kyzylool@corp.mail.ru>
2020-12-17 10:38:44 +0300

    centrifuge on

--- src/controller/message/index.ts
+++ src/controller/message/index.ts
@@ -1,7 +1,7 @@
 import {path, request, responsesAll, summary} from 'koa-swagger-decorator';
 import {BaseContext} from 'koa';
 import {addMessage, getMessages} from './methods/message';
-// import CentrifugeSingleton from '../centrifuge/centrifuge';
+import CentrifugeSingleton from '../centrifuge/centrifuge';
 
 @responsesAll({
 	200: {description: 'success'},
@@ -41,7 +41,7 @@ export default class MessageController {
 			try {
 				await addMessage(userId, chatId, text);
 
-				// await CentrifugeSingleton.getInstance().publish('news', text);
+				await CentrifugeSingleton.getInstance().publish('news', text);
 
 				ctx.status = 201;
 				return;
